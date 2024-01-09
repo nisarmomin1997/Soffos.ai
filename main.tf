207b98f95b11c6923754770af12de65ec1074eae
nisarmomin1997 <122545618+nisarmomin1997@users.noreply.github.com>
2024-01-09 19:21:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  credentials = file("<YOUR_SERVICE_ACCOUNT_KEY_FILE>")
+  project     = "<YOUR_PROJECT_ID>"
+  region      = "<YOUR_REGION>"
+}
+
+resource "google_container_cluster" "autopilot_cluster" {
+  name     = "autopilot-cluster"
+  location = "<YOUR_REGION>"
+
+
+  release_channel {
+    channel = "regular"
+  }
+
+  node_pool {
+    name = "default-pool"
+    config {
+      autopilot = true
+    }
+  }
+}
+
+output "kubeconfig" {
+  value = google_container_cluster.autopilot_cluster.master_auth.0.client_certificate_config[0].client_certificate
+}
+
+output "cluster_name" {
+  value = google_container_cluster.autopilot_cluster.name
+}
