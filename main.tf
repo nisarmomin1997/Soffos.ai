bf420478e1eba23a5128b25d5061e35f3abc315c
nisarmomin1997 <122545618+nisarmomin1997@users.noreply.github.com>
2024-01-09 20:55:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,44 @@
-#Terraform code to deploy a GKE Autopilot cluster
-
-provider "google" {
-  credentials = file("<YOUR_SERVICE_ACCOUNT_KEY_FILE>")
-  project     = "<YOUR_PROJECT_ID>"
-  region      = "<YOUR_REGION>"
+locals {
+  cluster_type           = "simple-autopilot-private"
+  network_name           = "simple-autopilot-private-network"
+  subnet_name            = "simple-autopilot-private-subnet"
+  master_auth_subnetwork = "simple-autopilot-private-master-subnet"
+  pods_range_name        = "ip-range-pods-simple-autopilot-private"
+  svc_range_name         = "ip-range-svc-simple-autopilot-private"
+  subnet_names           = [for subnet_self_link in module.gcp-network.subnets_self_links : split("/", subnet_self_link)[length(split("/", subnet_self_link)) - 1]]
 }
 
-resource "google_container_cluster" "autopilot_cluster" {
-  name     = "autopilot-cluster"
-  location = "<YOUR_REGION>"
 
+data "google_client_config" "default" {}
 
-  release_channel {
-    channel = "regular"
-  }
-
-  node_pool {
-    name = "default-pool"
-    config {
-      autopilot = true
-    }
-  }
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
 
-output "kubeconfig" {
-  value = google_container_cluster.autopilot_cluster.master_auth.0.client_certificate_config[0].client_certificate
-}
+module "gke" {
+  source                          = "../../modules/beta-autopilot-private-cluster/"
+  project_id                      = var.project_id
+  name                            = "${local.cluster_type}-cluster"
+  regional                        = true
+  region                          = var.region
+  network                         = module.gcp-network.network_name
+  subnetwork                      = local.subnet_names[index(module.gcp-network.subnets_names, local.subnet_name)]
+  ip_range_pods                   = local.pods_range_name
+  ip_range_services               = local.svc_range_name
+  release_channel                 = "REGULAR"
+  enable_vertical_pod_autoscaling = true
+  enable_private_endpoint         = true
+  enable_private_nodes            = true
+  master_ipv4_cidr_block          = "172.16.0.0/28"
+  network_tags                    = [local.cluster_type]
+  deletion_protection             = false
 
-output "cluster_name" {
-  value = google_container_cluster.autopilot_cluster.name
+  master_authorized_networks = [
+    {
+      cidr_block   = "10.60.0.0/17"
+      display_name = "VPC"
+    },
+  ]
 }
